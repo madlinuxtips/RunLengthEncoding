93f432e50ffa6f4f9d856ae47ec75c04f38f37fc
matty <mathewgibney2@gmail.com>
2019-03-21 22:35:04 +0000

    Test program for Encode and Decode

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.ArrayList;
@@ -7,19 +8,61 @@ public class Main {
 
   public static byte[] readFile(String filename) throws IOException {
     byte[] array={};
-    array = Files.readAllBytes(new File("repeats.txt").toPath());
+    array = Files.readAllBytes(new File(filename).toPath());
     return array;
+  }
+  public static void writeFile(String filename, byte[] writeBytes) throws IOException{
+    try (FileOutputStream stream = new FileOutputStream(filename)) {
+      stream.write(writeBytes);
+    }
   }
 
   public static void main(String[] args) {
-    byte array[] = {};
-    try{
-      array = readFile("repeats.txt");
+    if(args.length==3){
+      System.out.println(args[0]);
+      if(args[0].equals("encode")){
+        System.out.println("Inside");
+        byte[] readBytes = {};
+        try{
+          readBytes=readFile(args[1]);
+        }
+        catch (IOException e){
+          System.out.println("Cannot open file");
+        }
+        byte[] encodedBytes = Encode.encode(readBytes);
+        try{
+          writeFile(args[2],encodedBytes);
+        }
+        catch (IOException e){
+          System.out.println("Cannot open file");
+        }
+      }
+      else if(args[0].equals("decode")){
+        byte[] readBytes = {};
+        try{
+          readBytes=readFile(args[1]);
+        }
+        catch (IOException e){
+          System.out.println("Cannot open file");
+        }
+        byte[] decodedBytes = Decode.decode(readBytes);
+        try{
+          writeFile(args[2],decodedBytes);
+        }
+        catch (IOException e){
+          System.out.println("Cannot open file");
+        }
+
+      }
+      else{
+        System.out.println("Usage:java Main decode file");
+        System.out.println("Here");
+      }
     }
-    catch(IOException e){
-      System.out.println("The File cannot be opened");
+    else{
+      System.out.println("Usage:java Main decode file");
+      System.exit(1);
     }
-    ArrayList<Byte> bytes = Encode.encode(array);
-    System.out.println(bytes);
+
   }
 }
