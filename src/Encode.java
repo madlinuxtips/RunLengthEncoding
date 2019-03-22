849c70c909cde69389953a675c37e6bfc253d8aa
matty <mathewgibney2@gmail.com>
2019-03-22 00:42:32 +0000

    Tidy up Encode class

--- src/Encode.java
+++ src/Encode.java
@@ -1,26 +1,34 @@
-
 public class Encode {
   public static byte[] encode(byte[] readBytes) {
+
     byte[] writeBytes = new byte[readBytes.length];
+
+    //Repition count of current byte
     int count = 0;
+
+    //Keep track of smaller index
     int writeCount=0;
 
     for (int i = 0; i < readBytes.length - 1; i++) {
-
+      //Exit the program if the file is going to be larger than the original
+      if(writeCount > readBytes.length - 3){
+        System.out.println("File cannot be compressed with this algorithm");
+        System.exit(1);
+      }
       if (readBytes[i] == readBytes[i+1]) {
-
         count = 2;
 
         while (i < readBytes.length - 2) {
-
           i++;
 
           if (readBytes[i] == readBytes[i+1]) {
             count++;
           }
+
           else {
             break;
           }
+
         }
         writeBytes[writeCount]=(byte)count;
         writeCount++;
@@ -34,10 +42,12 @@ public class Encode {
         writeCount++;
       }
     }
+    //Remove null values at end of byte array
     byte[] cleanBytes = new byte[writeCount];
     for(int i=0;i<writeCount;i++){
       cleanBytes[i] = writeBytes[i];
     }
+
     return cleanBytes;
   }
 }
