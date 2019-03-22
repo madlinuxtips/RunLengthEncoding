0bef28ddb2baab0124cb0d084fa863a5ed35895e
matty <sc18msg@leeds.ac.uk>
2019-03-22 21:39:26 +0000

    Fixed last byte error

--- src/Encode.java
+++ src/Encode.java
@@ -1,6 +1,6 @@
 public class Encode {
-  public static byte[] encode(byte[] readBytes) {
 
+  public static byte[] encode(byte[] readBytes) {
     byte[] writeBytes = new byte[readBytes.length];
 
     //Repition count of current byte
@@ -9,32 +9,37 @@ public class Encode {
     //Keep track of smaller index
     int writeCount=0;
 
-    for (int i = 0; i < readBytes.length - 1; i++) {
+    for (int i = 0; i < readBytes.length ; i++) {
       //Exit the program if the file is going to be larger than the original
       if(writeCount > readBytes.length - 3){
         System.out.println("File cannot be compressed with this algorithm");
         System.exit(1);
       }
-      if (readBytes[i] == readBytes[i+1]) {
+
+      //Check we arent tring to access outside array
+      if (i!=readBytes.length-1 && readBytes[i] == readBytes[i+1]) {
         count = 2;
 
         while (i < readBytes.length - 2) {
+          //Move to next byte
           i++;
-
-          if (readBytes[i] == readBytes[i+1]) {
+          //Max size for signed int in a byte is 127
+          if (readBytes[i] == readBytes[i+1] && count < 127) {
             count++;
           }
-
           else {
             break;
           }
 
         }
+        //Write count of byte to array
         writeBytes[writeCount]=(byte)count;
         writeCount++;
+        //Write the byte to array
         writeBytes[writeCount]=readBytes[i];
         writeCount++;
       }
+      //Write a byte alone
       else {
         writeBytes[writeCount]=(byte)1;
         writeCount++;
@@ -47,7 +52,6 @@ public class Encode {
     for(int i=0;i<writeCount;i++){
       cleanBytes[i] = writeBytes[i];
     }
-
     return cleanBytes;
   }
 }
