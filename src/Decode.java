cde1d0c444cb0ca55868735ede9e2de873c33eb3
matty <mathewgibney2@gmail.com>
2019-03-22 00:44:13 +0000

    Tidy up Decode class

--- src/Decode.java
+++ src/Decode.java
@@ -4,25 +4,29 @@ public class Decode {
   public static byte[] decode(byte[] readBytes){
     ArrayList<Byte> newBytes = new ArrayList<>();
     boolean writeToggle=false;
-    int runlength=0;
+    int runLength=0;
 
     for(int i=0; i<readBytes.length;i++){
+
       if(writeToggle){
-        for(int j=0;j<runlength;j++){
+        for(int j=0;j<runLength;j++){
           newBytes.add(readBytes[i]);
         }
         writeToggle=false;
       }
+
       else{
-        runlength = (int)readBytes[i];
+        runLength = (int)readBytes[i];
         writeToggle=true;
       }
-
     }
+
+    //Turn Array list into byte array
     byte [] writeBytes = new byte[newBytes.size()];
     for(int i=0; i<newBytes.size();i++){
       writeBytes[i] =newBytes.get(i);
     }
+
     return writeBytes;
   }
 }
